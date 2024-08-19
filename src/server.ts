c4303e5cbb597c286e240bfa86b6924abc9ec23d
D-Vspec <dv@shamit.org>
2024-08-19 12:00:45 +0530

    Setup basic backend

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,25 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
+
+const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/mern_project')
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI as string)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+app.get('/', (req: Request, res: Response) => {
+    res.send('MERN Sustainability Project Backend');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
